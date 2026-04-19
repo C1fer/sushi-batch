cdb5ca6965bf6449609de6f4322cbb107ec51c85
C1fer <rusbelduarte@gmail.com>
2026-04-18 22:22:01 -0400

    feat: update destructive color hex code

--- sushi_batch/utils/prompts/confirm_prompt.py
+++ sushi_batch/utils/prompts/confirm_prompt.py
@@ -6,20 +6,12 @@ from prompt_toolkit.cursor_shapes import CursorShape
 from ..constants import COLOR_ACCENT, COLOR_DESTRUCTIVE
 from ..console_utils import print_error
 
-DEFAULT_STYLE = Style.from_dict({
-    "message" :f"fg:{COLOR_ACCENT} bold"
-})
-
-DESTRUCTIVE_STYLE = Style.from_dict({
-    "message" :f"fg:{COLOR_DESTRUCTIVE} bold"
-})
-
-
 def get(message="Are you sure?", suffix=" (Y/N): ", nl_before=False, nl_after=False, destructive=False, **kwargs):
     """Prompt user for a yes/no confirmation."""
     _message = [("class:message", f"> {message}{suffix}")]
-    style = DESTRUCTIVE_STYLE if destructive else DEFAULT_STYLE
+    _color = COLOR_DESTRUCTIVE if destructive else COLOR_ACCENT
 
+    kwargs.setdefault("style", Style([("message", f"fg:{_color} bold")]))
     kwargs.setdefault("cursor", CursorShape.BLOCK)
    
     if nl_before and not message.strip().startswith("\n"):
@@ -30,7 +22,7 @@ def get(message="Are you sure?", suffix=" (Y/N): ", nl_before=False, nl_after=Fa
             print()
 
     while True:
-        user_input = prompt(_message, style=style, **kwargs).upper()
+        user_input = prompt(_message, **kwargs).upper()
         match user_input:
             case "Y":
                 _print_new_line_after()
